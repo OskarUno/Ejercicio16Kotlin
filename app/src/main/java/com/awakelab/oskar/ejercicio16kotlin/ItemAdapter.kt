eec5de5b80d3b0768e71ea6782c834edbfe0d0fc
OskarUno <oorellana80@gmail.com>
2023-07-19 22:30:31 -0400

    Ejercicio 16 Kotlin, se crea vista de clickOnListener para los item

--- app/src/main/java/com/awakelab/oskar/ejercicio16kotlin/ItemAdapter.kt
+++ app/src/main/java/com/awakelab/oskar/ejercicio16kotlin/ItemAdapter.kt
@@ -1,6 +1,7 @@
 package com.awakelab.oskar.ejercicio16kotlin
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
@@ -8,6 +9,7 @@ import com.awakelab.oskar.ejercicio16kotlin.databinding.ItemLayoutBinding
 
 class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     var paises = mutableListOf<Pais>()
+    var callback: PaisCallback? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemLayoutBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
@@ -21,19 +23,35 @@ class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = paises[position]
         holder.bind(item)
+
     }
 
     fun setData(listaPaises: List<Pais>) {
         this.paises = listaPaises.toMutableList()
     }
 
-    class ViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(pais: Pais) {
             binding.textView.text = pais.nombre
             binding.imageView.load(pais.imgUrl)
+            binding.cardView1.setOnClickListener(View.OnClickListener {
+                val poblacion = "Poblacion: ${pais.poblacion}"
+                val pais = "Pais: ${pais.nombre}"
+                callback?.showContry(poblacion, pais)
+
+                /*Toast.makeText(
+                     itemView.context,
+                     "Pais: ${pais.nombre}, Poblacion: ${pais.poblacion}",
+                     Toast.LENGTH_SHORT
+                 ).show()*/
+            })
         }
 
 
+    }
+
+    interface PaisCallback {
+        fun showContry(poblacion: String, pais: String)
     }
 
 }
--- app/src/main/java/com/awakelab/oskar/ejercicio16kotlin/MainActivity.kt
+++ app/src/main/java/com/awakelab/oskar/ejercicio16kotlin/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.awakelab.oskar.ejercicio16kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.awakelab.oskar.ejercicio16kotlin.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ItemAdapter.PaisCallback {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +17,14 @@ class MainActivity : AppCompatActivity() {
         val itemAdapter = ItemAdapter()
         val paises = PaisesLatam.paises
         itemAdapter.setData(paises)
+        itemAdapter.callback = this
         binding.RV.adapter = itemAdapter
     }
+
+    override fun showContry(poblacion: String, pais: String) {
+        binding.tvCiudad.text = poblacion
+        binding.tvPais.text = pais
+    }
+
+
 }
